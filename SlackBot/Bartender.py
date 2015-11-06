d0a41da0c0438128f359adcd707441392dfa63b0
Ian Hutchinson <ihutchinson@agalmicventures.com>
2015-11-05 18:11:58 -0600

    Split the SlackBot class into its own file

--- SlackBot/Bartender.py
+++ SlackBot/Bartender.py
@@ -2,97 +2,14 @@
 
 import random
 import re
-import slacksocket
 import sys
-import time
 
-class SlackBot:
-	"""
-	Base class which implements a lots of useful helpers.
-	"""
+from SlackBot import SlackBot
 
-	def __init__(self, token, translate=True):
-		self._token = token
-		self._translate = translate
-		self._idField = 'name' if translate else 'id'
-
-		self._socket = slacksocket.SlackSocket(token, translate=translate)
-
-	def userID(self):
-		"""
-		Returns the bot's user ID.
-		"""
-		return self._socket.user
-
-	def getChannel(self, channelID):
-		channels = [u for u in self._socket.loaded_channels['channels'] if u[self._idField] == channelID]
-		if len(channels) == 0:
-			if self._translate:
-				user = self.getUser(channelID)
-				if user is not None:
-					channels = [u for u in self._socket.loaded_channels['ims'] if u['user'] == user['id']]
-			else:
-				channels = [u for u in self._socket.loaded_channels['ims'] if u[self._idField] == channelID]
-
-		return channels[0] if len(channels) > 0 else None
-
-	def getUser(self, userID):
-		users = [u for u in self._socket.loaded_users if u[self._idField] == userID]
-		return users[0] if len(users) > 0 else None
-
-	def sendMessage(self, message, channelID):
-		"""
-		Sends a message to a channel as this bot.
-
-		:param message: str or [str]
-		:param channelID: str
-		:return: bool indicating success
-		"""
-		#Take a random message from a list
-		if type(message) is list:
-			message = random.sample(message, 1)[0]
-
-		self._socket.send_msg(message, channel_id=channelID)
-
-	def handleEvents(self, delay=0.1):
-		"""
-		Starts an infinite event handling loop.
-		"""
-		for event in self._socket.events():
-			self.handleEvent(event)
-		#while True:
-		#	#TODO: handle ConnectionResetError
-		#	events = self._client.rtm_read()
-		#
-		#	for event in events:
-		#		self.handleEvent(event)
-		#	time.sleep(delay)
-
-	def handleEvent(self, event):
-		"""
-		Handle a single event from the event stream.
-		"""
-		eventType = event.type
-		if eventType == 'hello':
-			self.handleHello(event)
-		elif eventType == 'message':
-			self.handleMessage(event)
-		#TODO: other event types
-
-	def handleHello(self, event):
-		"""
-		Handles a hello event (overriden by implementations).
-		"""
-
-	def handleMessage(self, event):
-		"""
-		Handles a hello event (overriden by implementations).
-		"""
-
-class Bartender(SlackBot):
+class Bartender(SlackBot.SlackBot):
 
 	def __init__(self, token):
-		SlackBot.__init__(self, token)
+		SlackBot.SlackBot.__init__(self, token)
 
 	def handleMessage(self, event):
 		eventJson = event.event
@@ -106,7 +23,6 @@ class Bartender(SlackBot):
 		channelID = eventJson['channel']
 		channel = self.getChannel(channelID)
 		isPrivate = channel.get('is_im', False)
-		print('Channel isPrivate: %s' % isPrivate)
 
 		text = eventJson['text']
 		strippedTokens = [t for t in re.split(r'\W', text.lower()) if t != '']
@@ -119,7 +35,7 @@ class Bartender(SlackBot):
 					'Hello <@%s>.' % userID,
 					'Greetings, <@%s>.' % userID,
 				]
-			elif strippedTokens[:2] == ['how', 'are', 'you']:
+			elif strippedTokens[:3] == ['how', 'are', 'you']:
 				responses = [
 					'Good.',
 					'Doing good.'
---
+++ SlackBot/SlackBot.py
@@ -0,0 +1,101 @@
+
+import random
+import slacksocket
+
+class SlackBot:
+	"""
+	Base class which implements a lots of useful helpers.
+	"""
+
+	def __init__(self, token, translate=True):
+		self._token = token
+		self._translate = translate
+		self._idField = 'name' if translate else 'id'
+
+		self._socket = slacksocket.SlackSocket(token, translate=translate)
+
+	def token(self):
+		"""
+		Returns the token.
+
+		:return: str
+		"""
+		return self._token
+
+	def userID(self):
+		"""
+		Returns the bot's user ID.
+
+		:return str
+		"""
+		return self._socket.user
+
+	def getChannel(self, channelID):
+		"""
+		Gets a channel from an ID.
+
+		:param channelID: str
+		:return dict (channel JSON)
+		"""
+		channels = [u for u in self._socket.loaded_channels['channels'] if u[self._idField] == channelID]
+		if len(channels) == 0:
+			if self._translate:
+				user = self.getUser(channelID)
+				if user is not None:
+					channels = [u for u in self._socket.loaded_channels['ims'] if u['user'] == user['id']]
+			else:
+				channels = [u for u in self._socket.loaded_channels['ims'] if u[self._idField] == channelID]
+
+		return channels[0] if len(channels) > 0 else None
+
+	def getUser(self, userID):
+		"""
+		Gets a user from an ID.
+
+		:param userID: str
+		:return dict (user JSON)
+		"""
+		users = [u for u in self._socket.loaded_users if u[self._idField] == userID]
+		return users[0] if len(users) > 0 else None
+
+	def sendMessage(self, message, channelID):
+		"""
+		Sends a message to a channel as this bot.
+
+		:param message: str or [str]
+		:param channelID: str
+		:return: bool indicating success
+		"""
+		#Take a random message from a list
+		if type(message) is list:
+			message = random.sample(message, 1)[0]
+
+		self._socket.send_msg(message, channel_id=channelID)
+
+	def handleEvents(self, delay=0.1):
+		"""
+		Starts an infinite event handling loop.
+		"""
+		for event in self._socket.events():
+			self.handleEvent(event)
+
+	def handleEvent(self, event):
+		"""
+		Handle a single event from the event stream.
+		"""
+		eventType = event.type
+		if eventType == 'hello':
+			self.handleHello(event)
+		elif eventType == 'message':
+			self.handleMessage(event)
+		#TODO: other event types
+
+	def handleHello(self, event):
+		"""
+		Handles a hello event (overriden by implementations).
+		"""
+
+	def handleMessage(self, event):
+		"""
+		Handles a hello event (overriden by implementations).
+		"""
